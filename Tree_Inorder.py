a772e60774cb3a6ccf7e198e545a2482ffb691fe
Varun Rathi <varun.rathi@jeevansathi.com>
2020-10-28 20:11:25 +0530

    Tree inorder

---
+++ Tree_Inorder.py
@@ -0,0 +1,41 @@
+# your code goes here
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.left = None
+        self.right = None
+
+
+def inorderTraversal(node):
+    output = []
+    inorderHelper(node, output)
+    print(*output, sep= ' ')
+
+
+def inorderHelper(node, output):
+    if node is not None:
+        inorderHelper(node.left, output)
+        output.append(node.data)
+        inorderHelper(node.right, output)
+
+
+t = int(input())
+for _ in range(t):
+    N = int(input())
+    nums = [int(j) for j in input().split()]
+    nodes = []
+    for num in nums:
+        node = Node(num)
+        nodes.append(node)
+
+    for i in range(1, N):
+        inputArr = []
+        inputArr = [int(k) for k in input().split()]
+        parent, child, isRight = inputArr[0], inputArr[1], inputArr[2]
+        if isRight == 0:
+            nodes[parent].left = nodes[child]
+        else:
+            nodes[parent].right = nodes[child]
+
+    root = nodes[0]
+    inorderTraversal(root)
